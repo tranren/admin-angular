748583ff90a1b4f630011d791b0b3e56c94704a8
renyubin <tranren@126.com>
2017-07-10 19:33:34 +0800

    button component examples demonstrate

--- src/app/views/component_preview/buttons/buttons.component.ts
+++ src/app/views/component_preview/buttons/buttons.component.ts
@@ -3,145 +3,46 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.css']
+  styleUrls: ['./buttons.component.scss']
 })
 export class ButtonsComponent implements OnInit {
 
-  btns: Object;
+  btnName: string;
+  btnClass: string;
+  btnClick: Function;
+  btnDisabled: boolean;
+  previewConfig: Config;
 
   constructor() {
-
   }
 
   ngOnInit() {
-    this.btns = {
-      default: [
-        new ButtonConfig('default', 'btn-block btn-default', function () {
+    this.btnName = '按钮';
+    this.btnClass = 'btn-default';
+    this.btnClick = function () {
+      alert('这是一个按钮');
+    };
+    this.btnDisabled = false;
 
-        }),
-        new ButtonConfig('default', 'btn-block btn-default btn-lg', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-default btn-sm', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-default btn-xs', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-default btn-flat', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-default disabled', function () {
-
-        }),
-      ],
-      primary: [
-        new ButtonConfig('default', 'btn-block btn-primary', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-primary btn-lg', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-primary btn-sm', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-primary btn-xs', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-primary btn-flat', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-primary disabled', function () {
-
-        }),
-      ],
-      success: [
-        new ButtonConfig('default', 'btn-block btn-success', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-success btn-lg', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-success btn-sm', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-success btn-xs', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-success btn-flat', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-success disabled', function () {
-
-        }),
-      ],
-      info: [
-        new ButtonConfig('default', 'btn-block btn-info', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-info btn-lg', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-info btn-sm', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-info btn-xs', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-info btn-flat', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-info disabled', function () {
-
-        }),
-      ],
-      danger: [
-        new ButtonConfig('default', 'btn-block btn-danger', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-danger btn-lg', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-danger btn-sm', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-danger btn-xs', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-danger btn-flat', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-danger disabled', function () {
-
-        }),
-      ],
-      warning: [
-        new ButtonConfig('default', 'btn-block btn-warning', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-warning btn-lg', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-warning btn-sm', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-warning btn-xs', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-warning btn-flat', function () {
-
-        }),
-        new ButtonConfig('default', 'btn-block btn-warning disabled', function () {
-
-        }),
-      ]
-    }
+    this.previewConfig = new Config(false, 'btn-default', false);
   }
 
+  _onChange(e) {
+    this.previewConfig.isBlock = e.target.checked;
+    this.btnClass = (this.previewConfig.isBlock ? 'btn-block' : '') + ' ' + this.previewConfig.btnclass;
+  }
+
+  _btnRadio(btnClass) {
+    this.btnClass = btnClass + ' ' + (this.previewConfig.isBlock ? 'btn-block' : '');
+  }
+
+  _btnDisabled(e) {
+    this.btnDisabled = e.target.checked;
+  }
 }
 
-export class ButtonConfig {
+export class Config {
 
-  constructor(public btnName: string, public btnClass: string, public btnClick: Function) {
+  constructor(public isBlock: boolean, public btnclass: string, public btnDisabled: boolean) {
   }
 }
--- src/app/views/product.router.ts
+++ src/app/views/product.router.ts
@@ -3,5 +3,6 @@ import {TestComponent} from './product/test/test.component';
 import {ButtonsComponent} from './component_preview/buttons/buttons.component'
 
 export const childRoutes: Routes = [
+  {path: 'test', component: TestComponent},
   {path: 'buttons', component: ButtonsComponent}
 ]
