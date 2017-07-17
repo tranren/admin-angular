56ffe0146bcb47a98cafdfef585175e4135338cb
renyubin <renyubin@vacomall.com>
2017-07-18 00:39:16 +0800

    add route

--- src/app/views/product.router.ts
+++ src/app/views/product.router.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {ButtonsComponent} from './component_preview/buttons/buttons.component'
-
+import {FormComponent} from './component_preview/form/form.component'
 export const childRoutes: Routes = [
-  {path: 'buttons', component: ButtonsComponent}
+  {path: 'buttons', component: ButtonsComponent},
+  {path: 'form', component: FormComponent}
 ]
